0fa605215eb366709ea3bb82803b037e35d527b0
Michael Kolarik <kolarikm@mail.gvsu.edu>
2016-01-19 17:07:58 -0500

    Script now moves all image files from desktop to storage

--- desktop_cleaner/desk_clean.rb
+++ desktop_cleaner/desk_clean.rb
@@ -2,4 +2,13 @@
 
 require 'fileutils'
 
-FileUtils.mv('testing1', 'testdir/testing1')
+# Move images
+images = Dir.glob('/Users/Andromeda/Desktop/*.png') +
+	 Dir.glob('/Users/Andromeda/Desktop/*.jpg') +
+	 Dir.glob('/Users/Andromeda/Desktop/*.jpeg') +
+	 Dir.glob('/Users/Andromeda/Desktop/*.gif')
+puts "Moved images:"
+puts images.entries
+FileUtils.mv images, '/Users/Andromeda/Files/Images'
+
+
