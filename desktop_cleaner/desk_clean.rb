4c10afbcd47f9bfef7a1b06d7ea3b94e6e70b7c1
Michael Kolarik <kolarikm@mail.gvsu.edu>
2016-01-19 18:27:06 -0500

    Handles images, documents, bookmarks, and drops any other file into a miscellaneous folder.

--- desktop_cleaner/desk_clean.rb
+++ desktop_cleaner/desk_clean.rb
@@ -3,12 +3,24 @@
 require 'fileutils'
 
 # Move images
-images = Dir.glob('/Users/Andromeda/Desktop/*.png') +
-	 Dir.glob('/Users/Andromeda/Desktop/*.jpg') +
-	 Dir.glob('/Users/Andromeda/Desktop/*.jpeg') +
-	 Dir.glob('/Users/Andromeda/Desktop/*.gif')
+images = Dir.glob('/Users/Andromeda/Desktop/*.{png,jpg,jpeg,gif,bmp,gifv,webm}')
 puts "Moved images:"
 puts images.entries
 FileUtils.mv images, '/Users/Andromeda/Files/Images'
 
+# Move documents
+docs = Dir.glob('/Users/Andromeda/Desktop/*.{txt,docx,rtf,pdf,doc}')
+puts "Moved documents:"
+puts docs.entries
+FileUtils.mv docs, '/Users/Andromeda/Files/Documents'
 
+# Move bookmarks
+sites = Dir.glob('/Users/Andromeda/Desktop/*.webloc')
+puts "Moved bookmarks:"
+puts sites
+FileUtils.mv sites, '/Users/Andromeda/Files/Sites'
+
+misc = Dir.glob('/Users/Andromeda/Desktop/*')
+puts "Moved files:"
+puts misc
+FileUtils.mv misc, '/Users/Andromeda/Files/Misc'
